f97bb54c8d22e06d87ae9e415d7f7f61187e2569
Ray <rayding7@vt.edu>
2019-10-19 00:24:41 -0400

    optimized program a bit. Like .24s runtime

--- ex1/ex1_I_O.c
+++ ex1/ex1_I_O.c
@@ -8,27 +8,18 @@ int main()
 			FILE *fptrout;
 			char c; //need
 
-			fptr = fopen("input_file.txt","r"); //make sure to make r+ when this works
+			fptr = fopen("input_file.txt","r+"); //make sure to make r+ when this works
 
-			if(fptr == NULL) //checking validity
+			fptrout = fopen("output_file.txt","w");
+			
+			if(fptr == NULL || fptrout == NULL) //checking validity
 			{
 						printf("Did not work/file cannot be opened\n");
 						return -1;
 			}
 			
-			fptrout = fopen("output_file.txt","w");
-			
-			if(fptrout == NULL)
-			{
-						printf("File cannot be written to...\n");
-						return -1;
-			}
-			
 			while(fscanf(fptr,"%s",&c) != EOF) //endoffile checking addressing
-			{
-						//printf("in the loop"); testing success
 						fprintf(fptrout,"%s ",&c);
-			}
 			
 			fclose(fptr);
 			fclose(fptrout);
