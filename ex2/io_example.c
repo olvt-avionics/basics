d927e64f95366b8019c6c60cb6320f7702d9a010
stevef11 <48190541+stevef11@users.noreply.github.com>
2019-10-27 17:35:20 -0400

    Update io_example.c

---
+++ ex2/io_example.c
@@ -0,0 +1,14 @@
+int main() {
+    ifstream inStream;
+    inStream.open("input_file.txt");
+    ofStream outFile
+    outFile.open("output_file.txt");
+    if (outFile == null || inStream == null) {
+        printf("Requested File could not be accessed\n");
+        return -1;
+    }
+    while () {
+        
+    }
+    
+}
