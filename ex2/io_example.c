863e3a5f655d1b4564dda79f8d9563bee87c1d7b
Janak99 <47249937+Janak99@users.noreply.github.com>
2019-11-08 11:50:42 -0500

    Paragraph on Valgrind

---
+++ ex2/io_example.c
@@ -0,0 +1,4 @@
+I chose the tool called Valgrind. This was my choice of performance analyzer for multiple reasons. First, the tool is supported
+on mac osx which was most important to me. Additionally there is functionality such that it can perform various forms 
+of analyses on the most common programming file types, including C filetypes. Also, this tool helps me check for memory leaks
+and helps with debugging.
