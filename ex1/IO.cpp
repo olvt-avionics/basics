db5ee5b3863bc77f7556d85b738a01e245a1ef33
deveilbert <devaneilbert18@vt.edu>
2019-11-08 00:54:11 -0500

    intput_file.txt to command prompt

---
+++ ex1/IO.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+using namespace std; 
+
+string value;
+int i;
+
+int main(void) 
+{ 
+    ifstream inFile;
+    inFile.open("input_file.txt");
+    if(!inFile.is_open()){
+        cout << "file cannot open";
+    }else{
+        while(!inFile.eof()){
+            inFile >> value;
+            cout << value << "\t";
+        }
+    }
+} 
