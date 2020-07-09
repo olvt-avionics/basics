aa8a99e14976bd5bec9a85ed46be2a76669f7ba5
Janak Majeethia <janakmajeethia@gmail.com>
2020-07-09 18:52:51 -0400

    Finished queue ex6 in cpp

---
+++ ex6/README.md
@@ -0,0 +1,2 @@
+#Queue Application
+This program uses a queue to store user input from the command line. The queue is then outputted at the end.
---
+++ ex6/ex6.cpp
@@ -0,0 +1,45 @@
+#include <cstdlib> 
+#include <iostream>
+#include <fstream> 
+#include <string>
+#include <queue>
+
+class Writer {
+    private:
+        std::queue<std::string> intern;
+    public:
+        Writer(){}
+        Writer(std::queue<std::string>& q){
+          //copy param q into intern here  
+        }
+        void add(std::string str){
+            intern.push(str);
+        }
+        void to_file(){
+        }
+        void to_console(){
+            while(!intern.empty()){
+                std::cout << intern.front() << "--";
+                intern.pop();
+            }
+        }
+};
+
+int main(int argc, char *argv[]){
+    Writer w;
+    std::cout << "Type lines delimeted by pressing enter. End program by typing just 'e' then enter." << std::endl;
+    std::string s;
+    bool run = true;
+    while(run){
+        getline(std::cin,s);
+        if(s == "e"){
+            run = false;
+        }
+        else {
+           w.add(s); 
+           std::cout << "Added " << s << " to queue" <<std::endl;
+        }
+    }
+    w.to_console();
+    return EXIT_SUCCESS;
+}
